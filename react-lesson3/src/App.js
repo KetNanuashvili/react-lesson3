56f520e756dd2c7d4d088aeec54df5706fb0fda9
Ketevan Nanuashvili <100858778+KetiNanuashvili@users.noreply.github.com>
2023-01-16 00:33:16 +0400

    add usestates

--- react-lesson3/src/App.js
+++ react-lesson3/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const[x, setX] = useState(0);
+  const[y, setY] = useState(0);
+  const clickHandler = ()=>{
+    setX(x+1);
+  }
+  const clickHandler1 = ()=>{
+    setY(y+1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        x:{x}
+        y:{y}
+        </h1>
+      <button onClick={clickHandler}>click me </button>
+      <button onClick={clickHandler1}>click me y</button>
     </div>
   );
 }
