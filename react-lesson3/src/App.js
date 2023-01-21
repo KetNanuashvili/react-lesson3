405258e860c73edb7b8b4369c0326823ce9caa4a
Ketevan Nanuashvili <100858778+KetiNanuashvili@users.noreply.github.com>
2023-01-21 12:54:25 +0400

    add useEffect

--- react-lesson3/src/App.js
+++ react-lesson3/src/App.js
@@ -1,26 +1,55 @@
 import './App.css';
 import React, { useState } from 'react';
 
-function App() {
-  const[x, setX] = useState(0);
-  const[y, setY] = useState(0);
-  const clickHandler = ()=>{
-    setX(x+1);
-  }
-  const clickHandler1 = ()=>{
-    setY(y+1);
-  }
+const { useEffect } = require("react");
 
+// function App() {
+//   const[x, setX] = useState(0);
+//   const[y, setY] = useState(0);
+//   const clickHandler = ()=>{
+//     setX(x+1);
+//   }
+//   const clickHandler1 = ()=>{
+//     setY(y+1);
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>
+//         x:{x}
+//         y:{y}
+//         </h1>
+//       <button onClick={clickHandler}>click me </button>
+//       <button onClick={clickHandler1}>click me y</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//mounting komponentis dabadeba - anu roca pirvelad daiseta domshi
+//updating =  komponenti ganaxlda, magalitad state sheicvala
+// unmounting - komponentis sikvdili, komponenti domidan qreba
+
+
+const App =()=>{
+  const [x, setX] = useState(0);
+  useEffect( () =>{
+    console.log("after render");
+  }, []);
+  console.log("just logging");
   return (
-    <div className="App">
-      <h1>
-        x:{x}
-        y:{y}
-        </h1>
-      <button onClick={clickHandler}>click me </button>
-      <button onClick={clickHandler1}>click me y</button>
+    <div>
+      <h1>x:(x)</h1>
+      <button 
+      onClick={()=>{
+        setX((prevX) => prevX +1);
+      }}
+      >
+        set x
+      </button>
     </div>
   );
-}
+};
 
 export default App;
