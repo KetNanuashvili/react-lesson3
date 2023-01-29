e10a1feffc14518852ccd005f1464cfaf4721996
Ketevan Nanuashvili <100858778+KetiNanuashvili@users.noreply.github.com>
2023-01-29 23:21:02 +0400

    useState and useEffect

--- react-lesson3/src/App.js
+++ react-lesson3/src/App.js
@@ -32,24 +32,71 @@ const { useEffect } = require("react");
 // unmounting - komponentis sikvdili, komponenti domidan qreba
 
 
+// const App =()=>{
+//   const [x, setX] = useState(0);
+//   useEffect( () =>{
+//     console.log("after render");
+//   }, []);
+//   console.log("just logging");
+//   return (
+//     <div>
+//       <h1>x:(x)</h1>
+//       <button 
+//       onClick={()=>{
+//         setX((prevX) => prevX +1);
+//       }}
+//       >
+//         set x
+//       </button>
+//     </div>
+//   );
+// };
+
+const generationUsers = ()=>[
+  {id:1, name:"keti"},
+  {id:2, name:"natia"},
+  {id:3, name:"tatia"},
+  {id:4, name:"ana"},
+  {id:5, name:"salome"},
+  {id:6, name:"giorgi"},
+  {id:7, name:"dato"},
+  {id:8, name:"nika"},
+  {id:9, name:"luka"},
+  {id:10, name:"avto"},
+];
+
 const App =()=>{
-  const [x, setX] = useState(0);
-  useEffect( () =>{
-    console.log("after render");
+  const [users, setUsers]=useState([]);
+
+  useEffect (()=>{
+    setUsers(generationUsers());
   }, []);
-  console.log("just logging");
+
+  useEffect (()=>{
+    document.title =`${users.length} users left`;
+  }, [users]);
+
+  const onDeleteUser =()=>{
+    const randomIndex = Math.floor(Math.random() * users.length);
+    setUsers((prevUsers)=>{
+      const newUsersArray = prevUsers.filter(
+        (_, index)=> index!==randomIndex
+      );
+      return newUsersArray
+    })
+  }
+
   return (
     <div>
-      <h1>x:(x)</h1>
-      <button 
-      onClick={()=>{
-        setX((prevX) => prevX +1);
-      }}
-      >
-        set x
-      </button>
+      {users.map((user)=> (
+        <React.Fragment key={user.id}>
+      <h1 >{user.name} </h1>
+      </React.Fragment>
+      ))}
+         <button onClick={onDeleteUser}> delete user</button>
     </div>
   );
-};
+}
+
 
 export default App;
